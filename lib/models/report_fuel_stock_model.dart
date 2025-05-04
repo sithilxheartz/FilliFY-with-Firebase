544aaf74097c9effee55b27a48900e75179e6f74
Sithila Herath <sithilaherath1090@gmail.com>
2025-05-04 15:42:45 +0530

    created oil shop sales report part 2

--- lib/models/report_fuel_stock_model.dart
+++ lib/models/report_fuel_stock_model.dart
@@ -1,34 +1,3 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
-
-// class FuelStock {
-//   final String id;
-//   final String fuelType;
-//   final double addedQuantity;
-//   final Timestamp date;
-
-//   FuelStock({
-//     required this.id,
-//     required this.fuelType,
-//     required this.addedQuantity,
-//     required this.date,
-//   });
-
-//   // Factory constructor to convert Firestore document data into a FuelStock object
-//   factory FuelStock.fromFirestore(Map<String, dynamic> data, String id) {
-//     return FuelStock(
-//       id: id,
-//       fuelType:
-//           data['fuelType'] ??
-//           'Unknown', // Default value 'Unknown' if fuelType is null
-//       addedQuantity:
-//           data['addedQuantity']?.toDouble() ??
-//           0.0, // Ensure it's a double, default to 0.0
-//       date:
-//           data['date'] ??
-//           Timestamp.now(), // Default to current timestamp if date is null
-//     );
-//   }
-// }
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FuelStock {
