c4d4a6532537ef54a493d36ce01c9c4f313fb6f3
Sithila Herath <sithilaherath1090@gmail.com>
2025-05-12 11:37:47 +0530

    updated form validations

--- lib/oil_shop_module/customer_register_page.dart
+++ lib/oil_shop_module/customer_register_page.dart
@@ -182,30 +182,11 @@ class _CustomerRegisterPageState extends State<CustomerRegisterPage> {
                       return 'Please enter your password';
                     }
 
-                    // Check for a minimum length (e.g., 8 characters)
-                    if ((value?.length ?? 0) < 8) {
-                      return 'Password must be at least 8 characters long';
+                    // Check for a minimum length (e.g., 10 characters)
+                    if ((value?.length ?? 0) < 10) {
+                      return 'Mobile Number must be at least 10 digits';
                     }
 
-                    // Check for at least one lowercase letter
-                    if (!RegExp(r'[a-z]').hasMatch(value!)) {
-                      return 'Password must contain at least one lowercase letter';
-                    }
-
-                    // Check for at least one uppercase letter
-                    if (!RegExp(r'[A-Z]').hasMatch(value)) {
-                      return 'Password must contain at least one uppercase letter';
-                    }
-
-                    // Check for at least one digit
-                    if (!RegExp(r'\d').hasMatch(value)) {
-                      return 'Password must contain at least one digit';
-                    }
-
-                    // Check for at least one special character
-                    if (!RegExp(r'[!@#$%^&*(),.?":{}|<>]').hasMatch(value)) {
-                      return 'Password must contain at least one special character';
-                    }
                     return null;
                   },
                 ),
